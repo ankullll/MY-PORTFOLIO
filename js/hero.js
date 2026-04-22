739f1f822a25a24d88da024eb0319fa5dbb9be2e
Ankullll <ankulsingh975@gmail.com>
2026-04-23 03:53:37 +0530

    Added final touch to css and added sone js

---
+++ js/hero.js
@@ -0,0 +1,43 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const isHomePage = document.querySelector(".page.home-page");
+  if (!isHomePage) return;
+  gsap.registerPlugin(ScrollTrigger);
+
+  const heroImg = document.querySelector(".hero-img img");
+  let currentImageIndex = 1;
+  const totalImages = 10;
+  let scrollTriggerInstance = null;
+
+  setInterval(() => {
+    currentImageIndex =
+      currentImageIndex >= totalImages ? 1 : currentImageIndex + 1;
+    heroImg.src = `/public/images/work-items/work-item-${currentImageIndex}.jpg`;
+  }, 250);
+
+  const initAnimation = ()=>{
+    if(scrollTriggerInstance){
+        scrollTriggerInstance.kill();
+
+    }
+    scrollTriggerInstance = ScrollTrigger.create({
+        trigger:".hero-img-holder",
+        start:"top bottom",
+        end:"top top",
+        onUpdate :(self)=>{
+            const progress = self.progress;
+            gsap.set(".hero-img",{
+                y:`${-95 + 95*progress}%`,
+                scale:0.23+ 0.77*progress,
+                rotation:-15 +15*progress
+            })
+        }
+    })
+  }
+  initAnimation()
+  window.addEventListener("resize",()=>{
+    initAnimation()
+  })
+});
